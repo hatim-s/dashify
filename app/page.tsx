e96e67bf5de671b18e4ed4aa3d748f2d6698bc6a
Hatim Shakir <hatimshakir9179@gmail.com>
2024-11-02 18:17:36 +0530

    chore: add base theme

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/ui/components/button";
 export default async function Index() {
   return (
     <main className="flex-1 flex flex-col gap-6 px-4">
-      <Button>hello world</Button>
+      <Button variant="default">hello</Button>
     </main>
   );
 }
